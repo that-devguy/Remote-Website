c2ffe1361390baf2cf6f5d15d1af29ca0ed0c2a5
that devguy <zkmutch@gmail.com>
2023-03-22 20:54:13 -0400

    worked on form functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,31 @@ let degreeDevelopment = document.getElementById('degree-of-development');
 let targetCostDiv = document.getElementById('target-cost-div');
 let annualVolDiv = document.getElementById('annual-vol-div');
 let launchDiv = document.getElementById('launch-div');
+let materialsDiv = document.getElementById('materials-div');
+let quantityDiv = document.getElementById('quantity-div');
 
+// dynamically changes inputs based on selected services
 selectService.addEventListener('change', function() {
     let selectedOption = this.value;
     if (selectedOption === "design-needs") {
         designNeedsDiv.classList.remove("d-none");
+        materialsDiv.classList.add("d-none");
+        quantityDiv.classList.add("d-none");
     } else if (selectedOption === "additive-manufacturing") {
         designNeedsDiv.classList.add("d-none");
         targetCostDiv.classList.add("d-none");
         annualVolDiv.classList.add("d-none");
         launchDiv.classList.add("d-none");
+        materialsDiv.classList.remove("d-none");
+        quantityDiv.classList.remove("d-none");
     } else if ( selectedOption === "both") {
         designNeedsDiv.classList.remove("d-none");
+        materialsDiv.classList.remove("d-none");
+        quantityDiv.classList.remove("d-none");
     }
 })
 
+// dynamically changes the production ready group based on whether or not the user has selected production ready in the degree of development
 degreeDevelopment.addEventListener('change', function() {
     let selectedOption = this.value;
     if (selectedOption === "production-ready") {
@@ -31,3 +41,4 @@ degreeDevelopment.addEventListener('change', function() {
         launchDiv.classList.add("d-none");
     }
 })
+
